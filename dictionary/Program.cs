7fccc9dac9106688d9124204d38ad465ab9add0f
Wahyu Trisvianto <wahyu.trisvianto@gmail.com>
2017-05-24 11:56:48 +0700

    dictionary
    Learning how to use dictionary in C#

---
+++ dictionary/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+			////deklarasi output dalam bentuk data collection list
+			//List<string> output = new List<string>();
+
+			////karena input berupa kata, harus diparsing ke tiap huruf
+			//foreach (char huruf in input.ToUpper())
+			//{
+			//	string morseCode = _textToMorse[huruf];
+			//	output.Add(morseCode);
+			//}
+
+            //Console.WriteLine(string.Join(" ", output));
+            Console.WriteLine(toMorse(input));
+        }
+
+        public static string toMorse(string input)
+        {
+            //deklarasi output dalam bentuk data collection list
+            List<string> output = new List<string>();
+
+            //karena input berupa kata, harus diparsing ke tiap huruf
+            foreach (char huruf in input.ToUpper()){
+                string morseCode = _textToMorse[huruf];
+                output.Add(morseCode);
+            }
+
+            return string.Join(" ",output);
+        }
+
+		// in MorseCodeTranslator class
+		private static Dictionary<char, string> _textToMorse = new Dictionary<char, string>()
+		{
+			{' ', "/"},
+			{'A', ".-"},
+			{'B', "-..."},
+			{'C', "-.-."},
+			{'D', "-.."},
+			{'E', "."},
+			{'F', "..-."},
+			{'G', "--."},
+			{'H', "...."},
+			{'I', ".."},
+			{'J', ".---"},
+			{'K', "-.-"},
+			{'L', ".-.."},
+			{'M', "--"},
+			{'N', "-."},
+			{'O', "---"},
+			{'P', ".--."},
+			{'Q', "--.-"},
+			{'R', ".-."},
+			{'S', "..."},
+			{'T', "-"},
+			{'U', "..-"},
+			{'V', "...-"},
+			{'W', ".--"},
+			{'X', "-..-"},
+			{'Y', "-.--"},
+			{'Z', "--.."},
+			{'1', ".----"},
+			{'2', "..---"},
+			{'3', "...--"},
+			{'4', "....-"},
+			{'5', "....."},
+			{'6', "-...."},
+			{'7', "--..."},
+			{'8', "---.."},
+			{'9', "----."},
+			{'0', "-----"},
+		};
+    }
+}
